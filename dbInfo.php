640f6bb3ca5964211f6e9120eba38ac3ab4f42a8
LiangY <liangyi19941213@gmail.com>
2016-05-23 21:17:47 +0200

    Database information
    For security reasons, I save database info in this file but not in getConnection() function.

---
+++ dbInfo.php
@@ -0,0 +1,5 @@
+<?php
+    $dbHost = 'localhost'; // define database host
+    $dbUser= 'ly';  // define database username
+    $dbPassword = 'lynone94';  // define database password
+    $dbName = 'db_meetingmanagement';  // define database name
